f0dbf9218aad1815ef70d1d8a865922c872adcf4
Jim Fowler <fowler@math.osu.edu>
2019-12-11 11:38:05 -0500

    Slightly cleaner code

--- collaborate.js
+++ collaborate.js
@@ -1,47 +1,40 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   let worksheet = new window.doenet.Worksheet();
   window.worksheet = worksheet;
-    
-  document.getElementById("reset").addEventListener("click", function() {
-    worksheet.globalState.players = [];
-  });
 
   let player1 = document.getElementById("player1");
   let player2 = document.getElementById("player2");
-
-  player1.addEventListener("input", function() {
-    worksheet.globalState.player1 = player1.value;
-    document.getElementById("sum").innerHTML =
-      (parseInt(player1.value) + parseInt(player2.value)).toString();
-  });
-
-  player2.addEventListener("input", function() {
-    worksheet.globalState.player2 = player2.value;
-    document.getElementById("sum").innerHTML =
-      (parseInt(player1.value) + parseInt(player2.value)).toString();
-  });    
   
-  worksheet.addEventListener( 'globalState', function(e, state) {
-    if (worksheet.globalState.players === undefined)
-      worksheet.globalState.players = [];
+  function update(globalState) {
+    // Remove children of #players
+    let node = document.getElementById("players");
+    let cNode = node.cloneNode(false);
+    node.parentNode.replaceChild(cNode, node);
 
-    if (worksheet.globalState.player1)
-      player1.value = worksheet.globalState.player1;
+    // Display a list of all players
+    for( const player of globalState.players ) {
+      let li = document.createElement("li");
+      if (player === worksheet.userId) {
+        li.appendChild( document.createTextNode(player + " (you!)" ) );
+      } else {
+        li.appendChild( document.createTextNode(player) );
+      }
+      cNode.appendChild( li );
+    }
 
-    if (worksheet.globalState.player2)
-      player2.value = worksheet.globalState.player2;
-
+    // Display the sum of the two boxes
     document.getElementById("sum").innerHTML =
       (parseInt(player1.value) + parseInt(player2.value)).toString();
 
     // The userId only gets populated after the browser fingerprint is computed
     if (worksheet.userId) {
-      if (worksheet.globalState.players.indexOf( worksheet.userId ) === -1) {
-        worksheet.globalState.players.push( worksheet.userId );
+      if (globalState.players.indexOf( worksheet.userId ) === -1) {
+        globalState.players.push( worksheet.userId );
       }
 
-      var n = worksheet.globalState.players.indexOf( worksheet.userId );
+      var n = globalState.players.indexOf( worksheet.userId );
 
+      // Disable the input boxes depending on who the player is
       player1.disabled = true;
       player2.disabled = true;      
       
@@ -53,21 +46,36 @@ window.addEventListener('DOMContentLoaded', (event) => {
         player2.disabled = false;
       }      
     }
+  }
 
-      
-    let node = document.getElementById("players");
-    let cNode = node.cloneNode(false);
-    node.parentNode.replaceChild(cNode, node);
-    
-    for( const player of worksheet.globalState.players ) {
-      let li = document.createElement("li");
-      if (player === worksheet.userId) {
-        li.appendChild( document.createTextNode(player + " (you!)" ) );
-      } else {
-        li.appendChild( document.createTextNode(player) );
-      }
-      cNode.appendChild( li );
-    }
+  // Clear all the players
+  document.getElementById("reset").addEventListener("click", function() {
+    worksheet.globalState.players = [];
+    update(worksheet.globalState);
+  });
+
+  // Edit the Player 1 box
+  player1.addEventListener("input", function() {
+    worksheet.globalState.player1 = player1.value;
+    update(worksheet.globalState);    
+  });
+
+  // Edit the Player 2 box
+  player2.addEventListener("input", function() {
+    worksheet.globalState.player2 = player2.value;
+    update(worksheet.globalState);        
+  });    
+
+  worksheet.addEventListener( 'globalState', function(e, state) {
+    if (worksheet.globalState.players === undefined)
+      worksheet.globalState.players = [];
+
+    if (worksheet.globalState.player1)
+      player1.value = worksheet.globalState.player1;
+
+    if (worksheet.globalState.player2)
+      player2.value = worksheet.globalState.player2;
+
+    update(state);
   });  
-
 });
